f01b22ef0cb272c6f66d30125453e4d9578fd7b5
jpardo1-svg <j.pardo1@lanuevaamerica.edu.co>
2026-02-09 20:44:49 -0500

    Add AboutUs component with initial content

---
+++ AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h2 className="about-us-heading">Acerca de Paradise Nursery</h2>
+      <p className="about-us-description">En Paradise Nursery, nos apasiona llevar la naturaleza a tu hogar...</p>
+      <p className="about-us-content">Nuestra misión es proporcionar plantas de alta calidad...</p>
+    </div>
+  );
+}
+export default AboutUs;
